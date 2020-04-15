d94bd9df158a3a3f599642073b8571f7db1a86dd
sugijo <sugijo>
2020-04-15 17:11:44 +0900

    Initial Commit

---
+++ serihutest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  serihutest
+//
+//  Created by JotaroSugiyama on 2020/04/15.
+//  Copyright © 2020 JotaroSugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
